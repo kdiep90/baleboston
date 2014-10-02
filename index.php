a184307d022b612ab3d459c4ad1dc9189708c7d4
Uthedal <diep90@gmail.com>
2014-10-02 13:46:50 -0400

    Create index.php
    Home Page

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php include 'header.php' ?>
+	<!-- +++++ Welcome Section +++++ -->
+	    <div class="container pt">
+			<div class="row">
+				<div class="col-lg-10 col-lg-offset-1 centered">
+					<h1>Welcome to Banh Mi Ba Le Boston!</h1>
+					<img src="assets/img/FINAL/IMG_0349.jpg" id="indeximg" alt="img" />
+					<p id="storeinfo">1052 Dorchester Ave<br>Dorchester, MA 02125<br>Open daily 5:00AM-8:00PM<br><a href="tel:6172657171" id="bannerphone">(617) 265-7171</a></p>
+					<p>We are a small family business that loves to provide food for the local community. We opened in 1996 in Boston and customer demand has gotten so high that we also had to open another location in Quincy in 2006!</p>
+					<p>We were voted #1 by Boston Magazine, the Boston Globe, and many other publications for food quality, taste, and customer service. We not only have the best Vietnamese food in Boston, but we also have great vegan food, scrumptious desserts, fresh-squeezed sugar cane juice, and more! We even have our very own famous artisan French baguette created due to popular customer demand! We are the best Vietnamese sandwich shop in Boston, but don't just take our word for it. Check out our review on <a href="http://www.seriouseats.com/2013/01/a-sandwich-a-day-the-awesome-banh-mi-from-ba-le-dorchester-ma.html?ref=search"><strong>Serious Eats</strong></a>!</p>
+					<p>So what are you waiting for? Don’t take our word for it. Come by and try our food and we are confident that you will agree that we have the best food in the area. We are located at 1052 Dorchester Ave, Dorchester, MA 02125 and open from 5:00AM-8:00PM.</p>
+					<h4>Come check out our new drinks and smoothies section!<br>Having a party or an event? Ba Le offers full catering services!</h4>
+				
+				</div><!-- /col-lg-8 -->
+			</div><!-- /row -->
+	    </div> <!-- /container -->
+
+	
+<?php include 'footer.php' ?>
