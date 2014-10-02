0d704d664416673f414f80a35b316e745c260a9d
Uthedal <diep90@gmail.com>
2014-10-02 14:05:03 -0400

    Create gallery.php
    Image gallery, supported by slim-box

---
+++ gallery.php
@@ -0,0 +1,49 @@
+<?php include 'header.php' ?>
+
+	<div class="container pt">
+		<div class="row mt">
+			<div class="col-lg-8 col-lg-offset-2 centered">
+				<h1>Gallery</h1>
+				<p>Check us out on VietFaceTV!</p>
+				<iframe width="95%" height="310" src="//www.youtube.com/embed/VgdlitEmx0A" frameborder="0" allowfullscreen></iframe>
+			</div>
+		</div>
+		<div class="row mt centered">
+			<h3 style="margin-top:0;">Commendations</h3>
+			<p>Check out what others have to say about us!</p>
+			<ul class="list-no-style" id="gallery">
+				<li><a href="assets/img/ba_le/boyscouts.JPG" rel="lightbox-review" title="Boyscout recognition"><img src="assets/img/thumbs/boyscouts.jpg" class="thumbnail wide-70px" /></a></li>
+				<li><a href="assets/img/ba_le/googlereview.JPG" rel="lightbox-review" title="Reviews from Google+"><img src="assets/img/thumbs/googlereview.jpg" class="thumbnail" /></a></li>
+				<li><a href="assets/img/ba_le/yelpreview.JPG" rel="lightbox-review" title="Praise from Yelp"><img src="assets/img/thumbs/yelpreview.jpg" class="thumbnail" /></a></li>
+			</ul>
+			<h3>Our Fare</h3>
+			<p>Here are some more images of our food to whet your appetite!</p>
+			<div class="col-lg-8 col-lg-offset-2">
+				<ul class="list-no-style" id="gallery">
+					<li><a href="assets/img/FINAL/small/IMG_0250.jpg" rel="lightbox-food" title=""><img src="assets/img/thumbs/IMG_0250.jpg" class="thumbnail wide-83px" /></a></li>
+					<li><a href="assets/img/FINAL/small/IMG_0261.jpg" rel="lightbox-food" title=""><img src="assets/img/thumbs/IMG_0261.jpg" class="thumbnail wide-74px" /></a></li>
+					<li><a href="assets/img/FINAL/small/IMG_0327.jpg" rel="lightbox-food" title=""><img src="assets/img/thumbs/IMG_0327.jpg" class="thumbnail" /></a></li>
+					<li><a href="assets/img/FINAL/small/IMG_0274.jpg" rel="lightbox-food" title=""><img src="assets/img/thumbs/IMG_0274.jpg" class="thumbnail wide-74px" /></a></li>
+					<li><a href="assets/img/FINAL/small/IMG_0322.jpg" rel="lightbox-food" title=""><img src="assets/img/thumbs/IMG_0322.jpg" class="thumbnail" /></a></li>
+					<li><a href="assets/img/FINAL/small/IMG_0382.jpg" rel="lightbox-food" title=""><img src="assets/img/thumbs/IMG_0382.jpg" class="thumbnail wide-74px" /></a></li>
+					<li><a href="assets/img/FINAL/small/IMG_0289.jpg" rel="lightbox-food" title=""><img src="assets/img/thumbs/IMG_0289.jpg" class="thumbnail wide-74px" /></a></li>
+					<li><a href="assets/img/FINAL/small/IMG_0292.jpg" rel="lightbox-food" title=""><img src="assets/img/thumbs/IMG_0292.jpg" class="thumbnail wide-74px" /></a></li>
+					<li><a href="assets/img/FINAL/small/IMG_0329.jpg" rel="lightbox-food" title=""><img src="assets/img/thumbs/IMG_0329.jpg" class="thumbnail" /></a></li>
+					<li><a href="assets/img/FINAL/small/IMG_0293.jpg" rel="lightbox-food" title=""><img src="assets/img/thumbs/IMG_0293.jpg" class="thumbnail wide-74px" /></a></li>
+					<li><a href="assets/img/FINAL/small/IMG_0335.jpg" rel="lightbox-food" title=""><img src="assets/img/thumbs/IMG_0335.jpg" class="thumbnail wide-74px" /></a></li>
+					<li><a href="assets/img/FINAL/small/IMG_0360.jpg" rel="lightbox-food" title=""><img src="assets/img/thumbs/IMG_0360.jpg" class="thumbnail" /></a></li>
+					<li><a href="assets/img/FINAL/small/IMG_0370.jpg" rel="lightbox-food" title=""><img src="assets/img/thumbs/IMG_0370.jpg" class="thumbnail wide-74px" /></a></li>
+					<li><a href="assets/img/FINAL/small/IMG_1124.jpg" rel="lightbox-food" title=""><img src="assets/img/thumbs/IMG_1124.jpg" class="thumbnail wide-65px" /></a></li>
+					<li><a href="assets/img/FINAL/small/IMG_1349.jpg" rel="lightbox-food" title=""><img src="assets/img/thumbs/IMG_1349.jpg" class="thumbnail wide-65px" /></a></li>
+					<li><a href="assets/img/FINAL/small/IMG_1372.jpg" rel="lightbox-food" title=""><img src="assets/img/thumbs/IMG_1372.jpg" class="thumbnail wide-65px" /></a></li>
+					<li><a href="assets/img/FINAL/small/IMG_1395.jpg" rel="lightbox-food" title=""><img src="assets/img/thumbs/IMG_1395.jpg" class="thumbnail wide-65px" /></a></li>
+					<li><a href="assets/img/FINAL/small/IMG_1424.jpg" rel="lightbox-food" title=""><img src="assets/img/thumbs/IMG_1424.jpg" class="thumbnail wide-65px" /></a></li>
+				</ul>
+			</div>
+		</div><!-- /row -->
+	</div><!-- /container -->
+
+	
+	<!-- +++++ Footer Section +++++ -->
+		
+<?php include 'footer.php' ?>
