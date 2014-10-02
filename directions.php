567fee305762c2561b0479d075da2fe9f9b77ec0
Uthedal <diep90@gmail.com>
2014-10-02 14:04:16 -0400

    Create directions.php
    Directions page

---
+++ directions.php
@@ -0,0 +1,37 @@
+<?php include 'header.php' ?>
+	
+	<div class="container pt">
+		<div class="row mt">
+			<div class="col-lg-8 col-lg-offset-2 centered">
+				<h1>Directions</h1>
+				<p>We are located on Dorchester Ave, between the MBTA red line stops JKF/UMass and Savin Hill Station.</p>
+				<img src="assets/img/ba_le/store.jpg" id="storefrontimg" alt="storefront" />
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-5 col-lg-offset-1">
+				<h3 class="centered">Public Transportation</h3>
+				<h5 class="centered" style="margin-top:20px;">From JFK/UMass:</h5>
+				<ol>
+					<li>Turn left when you exit the station.</li>
+					<li>Walk down to Dorchester Ave and take a left.</li>
+					<li>Continue down to 1052 Dorchester Ave. We will be on the left.</li>
+				</ol>
+				<h5 class="centered">From Savin Hill:</h5>
+				<ol>
+					<li>Exit onto Savin Hill Ave and take a left.</li>
+					<li>Walk down to Dorchester Ave and take a right.</li>
+					<li>Continue down to 1052 Dorchester Ave. We will be on the right.</li>
+				</ol>
+			</div>
+			<div class="col-lg-5 centered">
+				<h3 class="centered">Map</h3>
+				<iframe width="100%" height="320" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?t=m&amp;hl=en-US&amp;gl=US&amp;mapclient=embed&amp;q=1052+Dorchester+Ave+Boston,+MA+02125&amp;ie=UTF8&amp;hq=&amp;hnear=1052+Dorchester+Ave,+Boston,+Massachusetts+02125&amp;z=14&amp;ll=42.321679,-71.055153&amp;output=embed"></iframe>
+			</div>
+		</div>
+	</div>
+
+	
+	<!-- +++++ Footer Section +++++ -->
+	
+<?php include 'footer.php' ?>
