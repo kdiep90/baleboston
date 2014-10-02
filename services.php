46ae752b0d47c54439bc5e4ab66cc707e4b3ae08
Uthedal <diep90@gmail.com>
2014-10-02 13:59:11 -0400

    Create services.php
    Calls the csv reader and the menu-formatting pages to print out the menu

---
+++ services.php
@@ -0,0 +1,17 @@
+<?php include 'header.php' ?>
+
+	    <div class="container pt">
+			<div class="row mt">
+				<div class="col-lg-8 col-lg-offset-2 centered">
+					<h1>Menu</h1>
+					<p>Our food is of the freshest quality available and we take that very seriously. We prepare everything fresh right in front of you and you can customize it any way you like. Our friendly staff will make sure that everything is made exactly the way you want it. To top it all off, we can offer a complete, fresh, nutritious, delicious healthy meal for as little as $5!</p>
+					<?php include 'csv_reader.php' ?>
+					<?php include 'menu.php' ?>
+				</div>
+			</div>
+		</div>
+	
+	
+	<!-- +++++ Footer Section +++++ -->
+	
+<?php include 'footer.php' ?>
