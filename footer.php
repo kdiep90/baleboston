f8fdde9f7851dd1f77f181d2a4a5c3f68947aa75
Uthedal <diep90@gmail.com>
2014-10-02 13:55:11 -0400

    Create footer.php
    Business info at bottom of page and scripts called here.

---
+++ footer.php
@@ -0,0 +1,49 @@
+	<div id="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4">
+					<h4>Address</h4>
+					<address>1052 Dorchester Ave, <br>Dorchester MA 02125</address>
+					<p>Phone: <a href="tel:6172657171">(617) 265-7171</a></p>
+				</div><!-- /col-lg-4 -->
+				
+				<div class="col-lg-4">
+					<h4>Hours</h4>
+					<p>Oepn 7 Days a Week<br>5:00am - 8:00pm</p>
+				</div><!-- /col-lg-4 -->
+				
+				<div class="col-lg-4">
+					<h4>Social Media</h4>
+					<p>Check us out on <a href="http://www.yelp.com/biz/banh-mi-ba-le-dorchester">Yelp</a>, <a href="https://www.facebook.com/pages/Banh-Mi-Ba-Le/245211925557451">Facebook</a>, and <a href="https://foursquare.com/v/b%C3%A1nh-m%C3%AC-ba-le/4b5a441ef964a520adb828e3">Foursquare</a>!</p>
+				</div><!-- /col-lg-4 -->
+			
+			</div>
+		
+		</div>
+		<div class="container" style="margin-top:40px;">
+			<div class="row">
+				<p>Developed by <a href="http://smartonetechnology.com">Smart One Technology</a></p>
+			</div>
+		</div>
+	</div>
+	
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/js/hover.zoom-min.js"></script>
+    <script type="text/javascript" src="assets/js/hover.zoom.conf-min.js"></script>  
+    <script type="text/javascript" src="assets/slimbox-2.05/js/slimbox2.js"></script>
+    <script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-43319559-4', 'banhmibaleboston.com');
+		ga('send', 'pageview');
+	</script>
+
+</body>
+</html>
