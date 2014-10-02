d4ba77bd5ce1335cb82f3314d9582d4bef37756f
Uthedal <diep90@gmail.com>
2014-10-02 14:08:38 -0400

    Create contact.php
    Contact page, using BELLmailer for emails between customers and business.

---
+++ contact.php
@@ -0,0 +1,22 @@
+<?php include 'header.php' ?>
+
+	<!-- +++++ Contact Section +++++ -->
+	
+	<div class="container pt">
+		<div class="row mt">
+			<div class="col-lg-10 col-lg-offset-1 centered">
+				<h1>Contact Us</h1>
+				<div class="col-lg-6">
+					<img src="assets/img/FINAL/IMG_1050.jpg" id="contactimg" alt="contactimg" />
+					<p style="text-align: left;">If you'd like to get in touch with us, please give us a call at 617-265-7171. We are open every day of the week from 5:00am to 8:00pm. You can also reach us by e-mail by filling out the following form.</p>
+				</div>
+				<div class="col-lg-6">
+					<?php include 'BELLmailer.php' ?>
+				</div>	
+			</div> 			
+		</div><!-- /row -->
+	</div><!-- /container -->
+	
+	<!-- +++++ Footer Section +++++ -->
+	
+<?php include 'footer.php' ?>
