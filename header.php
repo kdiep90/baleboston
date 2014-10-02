16476ce24baffdd7709d966641ad8b204ddfa74b
Uthedal <diep90@gmail.com>
2014-10-02 13:51:28 -0400

    Create header.php
    Head and navigation

---
+++ header.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Banh Mi Ba Le Boston's website">
+    <meta name="author" content="kevin@smartonetechnology.com">
+    <meta name="keywords" content="banh mi ba le boston, ba le boston, ba le, banh mi, banhmi, vietnamese, vietnamese food, dessert, desserts, dorchester, quincy, fast food, sandwiches, vietnamese sandwiches"/>
+	<meta name="google-site-verification" content="fLj1vjZTmw3yYb5MD5_eu-GooYDq1HfWIGsqsKqLveo" />
+	
+    <link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
+
+    <title>Banh Mi Ba Le Boston</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/main-min.css" rel="stylesheet">
+    
+    <link rel="stylesheet" href="assets/slimbox-2.05/css/slimbox2.css" type="text/css" media="screen" />
+  
+
+
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+    <!-- Static navbar -->
+    <div class="navbar navbar-inverse navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php"><img id="headimg" src="assets/img/ba_le/Logo40percent.png" alt="homepage logo"></a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="services.php">Menu</a></li>
+            <li><a href="gallery.php">Gallery</a></li>
+            <li><a href="directions.php">Directions</a></li>
+            <li><a href="contact.php">Contact</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
